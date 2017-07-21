ea7c933319d5d01325dc58c0bb158cef82cb7bd3
Jon Litfin <jon.litfin@workstate.com>
2017-07-21 15:26:16 -0400

    Added event binding from current infographiclayout.tpl

--- cmap-2017-theme/src/js/infographics.js
+++ cmap-2017-theme/src/js/infographics.js
@@ -12,6 +12,19 @@
 		console.log('getDataValue');
 	}
 
+    function removeAllFade() {
+        var activeClass = "c3-defocused";
+        d3.selectAll('g').classed(activeClass, false);
+    }
+
+    function updateInfo() {
+        var thisID = $('.button-container').find('.on').attr('id');
+
+        $('.side-narrative').find('#' + thisID).addClass('display');
+        $('.mini-info:not(#' + thisID + ')').removeClass('display');
+
+    }    		
+
     var labelPosition = 'inner-middle';
     var axisPosition = false;
     var donutWidth = 45;
@@ -698,6 +711,137 @@
 		            return false;
 		        });
 		    }
+    	},
+    	bindEvents: function() {
+            
+    		//$(this).closest('parent').find('child') since we're looking in the section local to the button that was clicked.
+            $(".icon-info-white").click(function () {
+                $(this).closest(".info-icon-container").find('.icon-info-white').toggleClass('on');
+                $(this).closest('.chart-top-container').find('.left-info').toggleClass('hide');
+                $(this).closest('.chart-top-container').find('p.info-data').toggle();
+            });
+
+            $(".infographic-button").click(function () {
+                $(this).addClass('clicked');
+            });
+            
+            $(".infographic-button").on('click mouseenter', function () {
+                var thisID = $(this).attr('id');
+                var closestParent = $(this).closest('.infographic-section');
+                var chartClass = closestParent.find('#chart').attr('class');
+                //this only works for up to 5 categories... not sure of the intended behavior otherwise.
+                //could be remedied maybe by window.load function and normal jq. will try soon.
+                var firstChild = closestParent.find('g.c3-chart-lines').children(':nth-child(1)')[0];
+                var secondChild = closestParent.find('g.c3-chart-lines').children(':nth-child(2)')[0];
+                var thirdChild = closestParent.find('g.c3-chart-lines').children(':nth-child(3)')[0];
+                var fourthChild = closestParent.find('g.c3-chart-lines').children(':nth-child(4)')[0];
+                var fifthChild = closestParent.find('g.c3-chart-lines').children(':nth-child(5)')[0];
+                //we need to be more specific in our selection, to make sure we select the chart closest to the buttons,
+                //and apply the fade to probably nth-children depending on chart type.
+                if ($(window).width() < 420) {
+                    closestParent.find('.side-narrative.desktop-narrative').hide();
+
+                    if (closestParent.find('.icon-key-light').hasClass('inactive')) {
+                        closestParent.find('.side-narrative.m-side-narrative').show().find('#' + thisID).addClass('display');
+                    }
+                }
+                else {
+                    closestParent.find('.side-narrative.m-side-narrative').hide();
+                    if (!$(this).closest('infographic-button').find('.icon-paragraphh-white').hasClass('off')) {
+                        closestParent.find('.side-narrative.desktop-narrative').show().find('#' + thisID).addClass('display');
+                    }
+                }
+                if ($('.button-container').children().hasClass('on') != $(this)) {
+                    $('.button-container').children().removeClass('on');
+                    $(this).addClass('on');
+                    updateInfo();
+                }
+                else {
+                    $(this).addClass('on');
+                }
+                if ($(this).hasClass('on')) {
+                    if ($(this).is(":first-child")) {
+                        removeAllFade();
+                        //[0] to make sure we get the pure dom element for js .classList. 
+                        //jq wont select svg elements to add/remove classes. this is our workaround to preserve the classes existing there
+                        thirdChild.classList.add('c3-defocused');
+                        fourthChild.classList.add('c3-defocused');
+                        fifthChild.classList.add('c3-defocused');
+                        //put the addFade function here. 
+                        //will likely want an array of the children at some point, and pass those values to the function as before.
+                    }
+                    else if ($(this).is(":nth-child(2)")) {
+                        removeAllFade();
+                        firstChild.classList.add('c3-defocused');
+                        secondChild.classList.add('c3-defocused');
+                        fifthChild.classList.add('c3-defocused');
+                    }
+                    else if ($(this).is(":nth-child(3)")) {
+                        removeAllFade();
+                        firstChild.classList.add('c3-defocused');
+                        secondChild.classList.add('c3-defocused');
+                        thirdChild.classList.add('c3-defocused');
+                        fourthChild.classList.add('c3-defocused');
+                    }
+                    else {
+                        removeAllFade();
+                    }
+                }
+            });
+            
+            $(".infographic-button").on('mouseleave', function () {
+                var closestButton = $(this).closest('.infographic-button');
+                if (!$(this).hasClass('clicked')) {
+                    $(this).removeClass('on');
+                    removeAllFade();
+                    $('.side-narrative.desktop-narrative').hide();
+                    //remember to remove the 'off' class from the paragraph button.
+                    closestButton.find('.icon-paragraphh-white').removeClass('off');
+                }
+            });
+            
+            //need to know where we're clicking in terms of multiples... will need (this).closest(parent).find(child)
+            $('.icon-paragraphh-white').click(function () {
+                var container = $(this).closest('.btn-toggle');
+                if (container.hasClass('on')) {
+                    if ($(window).width() > 420) {
+                        $('.side-narrative.desktop-narrative').toggle();
+                        $(this).toggleClass('off');
+
+                    }
+                    else {
+                        $('.side-narrative.m-side-narrative').toggle();
+                    }
+                }
+                return false;
+            });
+            
+            //will need to have some way of finding which place we're in, so we dont close all of them.
+            $('.icon-close-white').click(function () {
+                if ($('.side-narrative').is(":visible")) {
+                    $('.side-narrative').hide();
+                }
+                var container = $(this).closest('.btn-toggle');
+                container.removeClass('on');
+                container.removeClass('clicked');
+                removeAllFade();
+                return false;
+            });
+            
+            $('.icon-paragraph-light, .icon-key-light').click(function () {
+                var legendParent = $(this).closest('.chart-legend');
+                $(this).closest('.m-legend-btns').find('.icon-paragraph-light, .icon-key-light').toggleClass('inactive');
+                legendParent.find('.legend-info').toggle();
+                if ($(this).closest('.m-legend-btns').find('.icon-paragraph-light').hasClass('inactive')) {
+                    legendParent.find('.side-narrative').hide();
+                }
+                else {
+                    legendParent.find('.side-narrative').show();
+                }
+                if ($('.button-container').children().hasClass('on')) {
+                    updateInfo();
+                }
+            });
     	}
     };
 }));
