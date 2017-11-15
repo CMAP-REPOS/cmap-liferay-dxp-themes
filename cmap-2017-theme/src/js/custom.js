bec124beaeb3672605010af13d067fc2f5016e21
Jon Litfin <jon.litfin@workstate.com>
2017-11-15 14:57:53 -0500

    YouTube updates Refactored findID to use regex (was returning empty value for some videos) Replaced template literals with String.replace()

--- cmap-2017-theme/src/js/custom.js
+++ cmap-2017-theme/src/js/custom.js
@@ -1,6 +1,6 @@
 var cmap = cmap || {};
 
-cmap.initSocialShare = function(container) {
+cmap.initSocialShare = function (container) {
   var shareLinks = '<div class="addthis_toolbox"><ul class="list-unstyled">' +
     '<li><a class="facebook addthis_button_facebook">Facebook</a>' +
     '<li><a class="twitter addthis_button_twitter" tw:via="GOTO2040">Twitter</a>' +
@@ -18,7 +18,7 @@ cmap.initSocialShare = function(container) {
       html: true,
       placement: 'bottom'
     })
-    .on('shown.bs.popover', function() {
+    .on('shown.bs.popover', function () {
       $('#social-bookmarks-container')
         .html(container.find('.addthis_toolbox')
           .clone()
@@ -33,7 +33,7 @@ cmap.initSocialShare = function(container) {
 
 cmap.global = {};
 
-cmap.global.sidenav = function() {
+cmap.global.sidenav = function () {
   // SIDE NAV WIDGET
   var sideNavOpen = false;
 
@@ -56,9 +56,9 @@ cmap.global.sidenav = function() {
       sideNavOpen = true;
     }
   });
-}
+};
 
-cmap.global.scrollnav = function() {
+cmap.global.scrollnav = function () {
   // SCROLL NAV
   var headerHeight = $('#main-header .nav-row-one').innerHeight();
   var $scrollNav = $('#scroll-nav');
@@ -72,18 +72,18 @@ cmap.global.scrollnav = function() {
   }
   checkForScrollNav();
   $(window).scroll(_.throttle(checkForScrollNav, 50));
-}
+};
 
-cmap.global.footerjumptotop = function() {
-  $('#jump-to-top').click(function() {
+cmap.global.footerjumptotop = function () {
+  $('#jump-to-top').click(function () {
     $('html,body').animate({
       scrollTop: 0
     }, 800);
   });
-}
+};
 
-cmap.global.youtube = function() {
-  $('.iframe-container iframe').each(function() {
+cmap.global.youtube = function () {
+  $('.iframe-container iframe').each(function () {
     var $this = $(this);
 
     function setHeight() {
@@ -94,26 +94,19 @@ cmap.global.youtube = function() {
     }
 
     function findID(src) {
-      var code_offset = src.indexOf('/embed/');
-      if (code_offset >= 0) {
-        // remove the beginning of the URL
-        var final = src.substring(code_offset + 7);
-
-        // finds params if they are included
-        var or = final.indexOf('?');
-        var and = final.indexOf('&');
-        var end = or > and ? and : or;
-
-        // remove the end of the URL
-        if (end) { final = final.substring(0, end); }
-        return final;
+      var re = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+      var match = src.match(re);
+      var videoId = '';
+      if (match && match.length === 3 && match[2].length === 11) {
+        videoId = match[2];
       }
+      return videoId;
     }
 
     // Builds the tagline in the DOM based on the API response
     function addVideoTagline(data) {
       var title = data.items[0].snippet.title;
-      var link = `https://youtu.be/${data.items[0].id}`;
+      var link = 'https://youtu.be/{id}'.replace('{id}', data.items[0].id);
       var $container = $('<div class="video-tagline"></div>');
       var $icon = $('<div class="icon"> <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18"> <g fill="#3C5976" fill-rule="evenodd"> <path fill-rule="nonzero" d="M9,17.5 C4.30585763,17.5 0.5,13.6941424 0.5,9 C0.5,4.30585763 4.30585763,0.5 9,0.5 C13.6941424,0.5 17.5,4.30585763 17.5,9 C17.5,13.6941424 13.6941424,17.5 9,17.5 Z M9,16.5 C13.1418576,16.5 16.5,13.1418576 16.5,9 C16.5,4.85814237 13.1418576,1.5 9,1.5 C4.85814237,1.5 1.5,4.85814237 1.5,9 C1.5,13.1418576 4.85814237,16.5 9,16.5 Z"/> <polygon points="6.8 5.75 6.8 12.25 12.8 8.75"/> </g> </svg> </div>');
       var $data = $('<div class="video-data"> <span class="whitney-normal"> Watch <a class="underline-link" href="' + link + '" target="_blank">' + title + '</a> on YouTube </span> </div>');
@@ -129,19 +122,19 @@ cmap.global.youtube = function() {
         key: "AIzaSyC7Ab6y-6mvks4oPwdc4vMkXoKFQXBsc5E",
         part: "snippet,statistics",
         id: vid
-      }, function(data) {
+      }, function (data) {
         if (data.items.length === 0) {
           console.error('YOUTUBE: ', 'Problem finding the video you want.');
           return;
         }
         addVideoTagline(data);
-      }).fail(function(jqXHR, textStatus, errorThrown) {
+      }).fail(function (jqXHR, textStatus, errorThrown) {
         console.error('YOUTUBE: ', 'Problem making a request to get video metadata.');
       });
     }
 
     setHeight();
-    $(window).resize(_.debounce(function() {
+    $(window).resize(_.debounce(function () {
       setHeight();
     }, 100));
 
@@ -150,7 +143,7 @@ cmap.global.youtube = function() {
   });
 };
 
-cmap.global.loginpage = function() {
+cmap.global.loginpage = function () {
   // LOGIN PAGE
   if ($('.portlet-login').length) {
     $('body').addClass('login-page');
@@ -161,39 +154,39 @@ cmap.global.loginpage = function() {
   }
 };
 
-cmap.global.checkforh1 = function(){
+cmap.global.checkforh1 = function () {
 
   // there should only be one h1 tag per page for SEO reasons
   // we are going to take some liferay h1s and convert them to <div>
-  function convertToDiv(element){
+  function convertToDiv(element) {
     var $this = $(element);
     var classes = $this.attr('class');
     var content = $this.text();
     $this.replaceWith(`<div class="${classes}">${content}</div>`);
   }
 
-  $('h1').each(function(){
-    if($(this).hasClass('hide-accessible')){
+  $('h1').each(function () {
+    if ($(this).hasClass('hide-accessible')) {
       convertToDiv(this);
     } else {
       console.log(this);
       cmap.global.scrollnavTitle($(this).text());
     }
   });
-}
+};
 
-cmap.global.scrollnavTitle = function(text){
+cmap.global.scrollnavTitle = function (text) {
   $('#scroll-nav .col-xl-13').append(`<h4>${text}</h4>`);
-}
+};
 
-cmap.global.addpageclass = function() {
+cmap.global.addpageclass = function () {
   var rawPath = Liferay.currentURL.substring(1);
   var noWebGuest = rawPath.replace(/web\/guest\//g, '');
   var noSlash = noWebGuest.replace(/\//g, '-');
   var final = '';
 
   if (noSlash === '') {
-    final = 'home-page'
+    final = 'home-page';
   } else {
     final = noSlash;
   }
@@ -202,13 +195,12 @@ cmap.global.addpageclass = function() {
   $('body').addClass(final);
 };
 
-
 cmap.forms = {};
 
-cmap.forms.contactus = function(){
+cmap.forms.contactus = function () {
 
   var $contact_form = $('.contact-us-page');
-  if($contact_form.length){
+  if ($contact_form.length) {
 
     // add section headers to form
     var $info_header = $(`
@@ -237,52 +229,52 @@ cmap.forms.contactus = function(){
     $('.lfr-ddm-form-submit').text('Send');
 
   }
-}
+};
 
-cmap.forms.global = function(){
+cmap.forms.global = function () {
 
   var $form = $('.portlet-forms');
-  if($form.length){ 
+  if ($form.length) {
 
     $form.find('.ddl-form-builder-app').removeClass('container-fluid-1280');
 
     var $form_info = $form.find('.ddl-form-basic-info');
-    if($form_info.length){
+    if ($form_info.length) {
 
       $form_title = $form_info.find('.ddl-form-name');
-      if($form_title.length){
+      if ($form_title.length) {
         $form_title.unwrap();
         $form_title.addClass('whitney-huge');
       }
       $form_description = $form_info.find('.ddl-form-description');
-      if($form_description.length){
+      if ($form_description.length) {
         $form_description.unwrap();
         $form_description.addClass('presna-normal');
       }
     }
 
     var $form_body = $form.find('.lfr-ddm-form-content');
-    if($form_body.length){
+    if ($form_body.length) {
 
       var $form_pages = $form_body.find('.lfr-ddm-form-page');
-      $form_pages.each(function(){
+      $form_pages.each(function () {
         var $page = $(this);
         var $required_warning = $page.find('.required-warning').remove();
-        
+
         $page.after($required_warning);
       });
     }
   }
 
-  $('input').change(function(){
-    if($(this).val() !== ''){
+  $('input').change(function () {
+    if ($(this).val() !== '') {
       $(this).addClass('has-value');
     }
   });
-}
+};
 
 // Runs when.. all the portlets are loaded and ready
-Liferay.on('allPortletsReady', function() {
+Liferay.on('allPortletsReady', function () {
 
   cmap.global.addpageclass();
   cmap.global.sidenav();
@@ -300,9 +292,9 @@ Liferay.on('allPortletsReady', function() {
 });
 
 // Runs once for each portlet on the page, with info about that portlet
-Liferay.Portlet.ready(function(portletId, node) {
+Liferay.Portlet.ready(function (portletId, node) {
   console.log(portletId, node);
 });
 
 // Runs once the DOM is finished. Better to use "allPortletsReady"
-$(document).ready(function() {});
+$(document).ready(function () { });
