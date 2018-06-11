5ccc92e0d8e62d5baf8eb7087e511fcdf868a1c4
Clark Nelson <mail@clarknelson.com>
2018-06-11 17:31:15 -0500

    happy with how anchors are turning out

--- cmap-onto-2050-theme/src/js/main.js
+++ cmap-onto-2050-theme/src/js/main.js
@@ -2,46 +2,58 @@
 
 window.cmap = window.cmap || {};
 window.cmap.global = window.cmap.global || {};
-window.cmap.global.anchors = window.cmap.global.anchors || function(){
-	$('.onto2050-basic-web-content a').each(parse_anchors);
-	function parse_anchors(el){
-	  var $this = $(this);
-		var attr_name = $this.attr('name');
-		var attr_id = $this.attr('id');
-		var name = $this.text();
-		var $button = $('<button class="page-anchor-button"><span class="sr-only">'+name+'</span><img class="page-anchor-icon" src="https://clarknelson.com/drop/ic_clipboard.svg" /></button>');
-		var url = window.location.href + '#' + attr_id;
 
-		$button.click(function(){
-			var $temp = $("<div>");
-		  $this.append($temp);
-		  $temp.attr("contenteditable", true)
-		       .html(url).select()
-		       .on("focus", function() { document.execCommand('selectAll',false,null) })
-		       .focus();
-		  document.execCommand("copy");
-		  $temp.remove();
-		});
-		// var $temp = $("<input>");
-		// $temp.val(attr_id);
-    // $("body").append($temp);
-		// console.log($this.attr('id'), attr_id, $temp);
-    // $temp.select();
-    // document.execCommand("copy");
-    // $temp.remove();
 
-	  // console.log($this, $this.attr('name'), $this.attr('id'));
-	  if(attr_name === attr_id){
-	    // console.log('ANCHOR TAG FOUND', $this);
-			$this.addClass('page-anchor');
-			$this.prepend($button);
-	  }
+window.cmap.global.anchors = window.cmap.global.anchors || {};
+window.cmap.global.anchors.crawl = window.cmap.global.anchors.crawl || function(){
+	var $this = $(this);
+	var attr_name = $this.attr('name');
+	var attr_id = $this.attr('id');
+	var name = $this.text();
+
+	var $button = $('<button class="page-anchor-button"><span class="sr-only">'+name+'</span><img class="page-anchor-icon" src="https://clarknelson.com/drop/ic_clipboard.svg" /></button>');
+	var $link = $('<li><h4><a href="#'+attr_id+'">'+name+'</a></h4></li>');
+
+	$link.click(function(e){
+		e.preventDefault();
+
+		var push = 0;
+		if(window.Liferay.ThemeDisplay.isSignedIn()){
+			push = push + $('.control-menu').innerHeight();
+		}
+		console.log(push, $('.control-menu').innerHeight());
+
+		$('html,body').animate({
+			scrollTop: $('#'+attr_id).offset().top - push
+		}, 1000);
+	});
+
+	function copy_link_to_clipboard(){
+		var $temp = $("<div>");
+		$this.append($temp);
+		$temp.attr("contenteditable", true)
+				 .html(window.location.origin + window.location.pathname + '#' + attr_id).select()
+				 .on("focus", function() { document.execCommand('selectAll',false,null) })
+				 .focus();
+		document.execCommand("copy");
+		$temp.remove();
 	}
+
+	if(attr_name === attr_id){
+		$('.page-nav .list-unstyled').append($link);
+		$button.click(copy_link_to_clipboard);
+		$this.click(copy_link_to_clipboard);
+		$this.addClass('page-anchor');
+		$this.prepend($button);
+	}
+}
+window.cmap.global.anchors.init = window.cmap.global.anchors.init || function(){
+	$('.onto2050-basic-web-content a').each(window.cmap.global.anchors.crawl);
 };
 
 window.cmap.global.init = window.cmap.global.init || function(){
 
-	window.cmap.global.anchors();
+	window.cmap.global.anchors.init();
 
 	// $('.breadcrumb-cmap .close-button').addClass('hidden');
 
